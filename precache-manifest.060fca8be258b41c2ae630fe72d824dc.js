30fdf9ff185061643a24df5981899338110474e5
wangj383 <57647231+wangj383@users.noreply.github.com>
2020-05-08 16:56:30 -0400

    Updates

---
+++ precache-manifest.060fca8be258b41c2ae630fe72d824dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cb1a355d4ce89e95b596625bdeacdb0",
+    "url": "/unit-4-assessment/index.html"
+  },
+  {
+    "revision": "b13548216a48775c3016",
+    "url": "/unit-4-assessment/static/css/main.7c87d728.chunk.css"
+  },
+  {
+    "revision": "cfb3cf804c5e963e704d",
+    "url": "/unit-4-assessment/static/js/2.7d230a0e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/unit-4-assessment/static/js/2.7d230a0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b13548216a48775c3016",
+    "url": "/unit-4-assessment/static/js/main.f409598b.chunk.js"
+  },
+  {
+    "revision": "87f25bd259a4c443f203",
+    "url": "/unit-4-assessment/static/js/runtime-main.7ee6aee7.js"
+  }
+]);
